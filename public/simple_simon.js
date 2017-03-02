732133c8c6a4abe9744066b0932d72a4010df79a
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-03-02 14:55:44 -0600

    Adds basic intro when red button is clicked for the first time

--- public/simple_simon.js
+++ public/simple_simon.js
@@ -2,6 +2,31 @@
 
 var gameState = "idle"
 
+function playIntro() {
+    var redOn = false;
+    var count = 0;
+    var intervalId = setInterval(function () {
+        if (count <= 50) {
+            if (!redOn) {
+                $("#red-button").addClass("lit");
+                redOn = true;
+            } else {
+                $("#red-button").removeClass("lit");
+                redOn = false;
+            }
+            count++;
+        } else {
+            console.log("Intro over.");
+            clearInterval(intervalId);
+        }
+    }, 35);
+}
+
 $("#red-button").click(function() {
     console.log("Button was clicked!");
+    gameState = "intro";
+    if (gameState == "idle") {
+        $("#red-button").removeClass("btn-enabled");
+        playIntro();
+    }
 });
